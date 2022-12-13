39d25fda4621b2b3345b4476d17756a2d54fec4c
TanyaMalina <105852395+TanyaMalina@users.noreply.github.com>
2022-12-13 18:53:35 +0300

    task 10 completed

--- FreeCodeCamp/taska_JS.js
+++ FreeCodeCamp/taska_JS.js
@@ -34,3 +34,9 @@ titleCaseOver = 9000;
 //9
 let catName = "Oliver";
 let catSound = "Meow!";
+
+//10
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
