d16c68e84e18a7bde4cb5bb5da69bae4945f79ee
TanyaMalina <105852395+TanyaMalina@users.noreply.github.com>
2022-12-14 21:27:11 +0300

    task 35 completed

--- FreeCodeCamp/tasks_31-40.js
+++ FreeCodeCamp/tasks_31-40.js
@@ -37,3 +37,12 @@
     myStr = "Hello World"; // Change this line
 // Only change code above this line
 }
+
+//35
+{
+    // Setup
+    const lastName = "Lovelace";
+
+// Only change code below this line
+    const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+}
