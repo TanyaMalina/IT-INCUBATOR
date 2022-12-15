658a8bf58df085a7d8652d7e99e1281e206b289a
TanyaMalina <105852395+TanyaMalina@users.noreply.github.com>
2022-12-15 20:54:28 +0300

    task 41 completed

--- FreeCodeCamp/tasks_31-40.js
+++ FreeCodeCamp/tasks_31-40.js
@@ -85,3 +85,11 @@
     const myArray = [50, 60, 70];
     const myData = myArray[0];
 }
+
+//41
+{
+    // Setup
+    const myArray = [18, 64, 99];
+    myArray[0] = 45;
+// Only change code below this line
+}
