b85950292e7ed18be03a305e344d043ce9ecb777
TanyaMalina <105852395+TanyaMalina@users.noreply.github.com>
2022-12-22 23:47:22 +0300

    task 82 completed

---
+++ FreeCodeCamp/task_82.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    "name": "Linda",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["People", "Cats"]
+
+    // Only change code above this line
+};
