aa2bfd9ba0d41de9d96feaa4deba557d63e85006
TanyaMalina <105852395+TanyaMalina@users.noreply.github.com>
2022-12-23 16:04:40 +0300

    task 91 completed

---
+++ FreeCodeCamp/task_91.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "Bob",
+        "title": "Piano",
+        "release_year": 1999,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ]
+    }
+];
